368b2cb497acb6b2156f154e9378dcdba2136ee8
nakmaura <nakamura>
2020-04-28 16:13:07 +0900

    fix app/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'items#index'
-  resources :items,only: :show
+  resources :items,only: [:new,:show]
   resources :purchases,only:[:new]
   resources :cards,only:[:new]
   resources :deliveries,only:[:new]
