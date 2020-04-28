5d87704ee0e3fb015fb9b1ba1f03f4fe0cf666ba
nakmaura <nakamura>
2020-04-28 15:03:14 +0900

    add routes /app.scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'items#index'
   resources :items,only: :show
+  resources :purchases,only:[:new]
+  resources :cards,only:[:new]
+  resources :deliveries,only:[:new]
 end
