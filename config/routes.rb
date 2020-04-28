339eb4f506a35e90c142f15eb02a724e69d1df10
tosiran <f2.ree_eat_10470@icloud.com>
2020-04-28 18:42:53 +0900

    商品詳細ページ　フロント実装完了
    コードレビュー依頼

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'items#index'
+  resources :items,only: [:new,:show]
+  resources :purchases,only:[:new]
+  resources :cards,only:[:new]
+  resources :deliveries,only:[:new]
 end
