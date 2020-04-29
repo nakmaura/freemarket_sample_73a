291f344b13016b1534e2aa5254a0561448bb1550
masayasu-ogasawara <ogasawara.masayasu@gmail.com>
2020-04-29 17:47:22 +0900

    devise_create_users db:migrate

--- db/migrate/20200429055130_devise_create_users.rb
+++ db/migrate/20200429055130_devise_create_users.rb
@@ -4,8 +4,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :password, null: false
+      t.string :re_password, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :first_name_kana, null: false
+      t.string :last_name_kana, null: false
+      t.date :birthday_date, null: false
 
       ## Recoverable
       t.string   :reset_password_token
