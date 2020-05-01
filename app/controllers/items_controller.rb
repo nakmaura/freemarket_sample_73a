4b411066eb7884f46bb06da6ca88a71f7d2aa957
nakmaura <nakamura>
2020-05-01 20:07:08 +0900

    fix 商品出品

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,40 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.includes(:images).order('created_at DESC')
   end
 
   def show
     
+  end
+
+  def new
+    @item = Item.new
+    @item.images.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    # Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, images_attributes: [:url])
+    # params.require(:item).permit(:name, :price,:url)
   end
 end
