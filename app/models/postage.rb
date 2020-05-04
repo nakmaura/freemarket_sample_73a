9181db271e020007367967c475034e240f5b5b4b
nakmaura <nakamura>
2020-05-04 14:54:36 +0900

    アソシエーション fix
    デプロイ修正のためアソシエーションをバックアップとったのち一旦削除

--- app/models/postage.rb
+++
@@ -1,9 +0,0 @@
-class Postage < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :postage_payer
-end
-
-class Postage < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefecture
-end
