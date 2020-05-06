d22c84c0990a21f3f71ddd9b57c972d50e3cb1bf
nakmaura <nakamura>
2020-05-06 15:32:18 +0900

    商品一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @blands = Item.includes(:images).where.not(bland: nil).where(buyer_id: nil).order("created_at DESC").limit(3)
+    @blands = Item.includes(:images).where.not(bland: "").where(buyer_id: nil).order("created_at DESC").limit(3)
     @newitems = Item.includes(:images).where(buyer_id: nil).order("created_at DESC").limit(3)
   end
 
@@ -34,6 +34,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name,:price,:introduction,:bland,:prefecture_name,:condition_id,:postage_payer,:preparation_day,images_attributes:{url:[]}).merge(seller_id:current_user.id)
+    params.require(:item).permit(:name,:price,:introduction,:bland,:prefecture_name,:condition_id,
+    :postage_payer,:preparation_day,images_attributes:[:url]).merge(seller_id:current_user.id)
   end
 end
